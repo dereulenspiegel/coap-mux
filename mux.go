a750328d5317cd4da9e568e4fd78b3954ea79564
Till Klocke <till.klocke@gmail.com>
2015-10-30 21:00:11 +0100

    Added some documentation for the Router

--- mux.go
+++ mux.go
@@ -28,6 +28,7 @@ func setVars(msg *coap.Message, vars map[string]string) {
 	mutex.Unlock()
 }
 
+// Var retrieves a named variable for this message
 func Var(msg *coap.Message, name string) string {
 	mutex.RLock()
 	var value = ""
@@ -46,15 +47,20 @@ func clearVars(msg *coap.Message) {
 	mutex.Unlock()
 }
 
+// Router handles routing CoAP Messages to the correct handlers. Currently
+// It doesn't support default routes etc.
 type Router struct {
 	NotFoundHandler coap.Handler
 	routes          []*Route
 }
 
+// NewRouter creates a new Router
 func NewRouter() *Router {
 	return &Router{routes: make([]*Route, 0, 50)}
 }
 
+// Match matches the incoming message against all routes. The first matching route
+// wins.
 func (r *Router) Match(msg *coap.Message, addr *net.UDPAddr, match *RouteMatch) bool {
 	for _, route := range r.routes {
 		if route.Match(msg, addr, match) {
@@ -64,20 +70,25 @@ func (r *Router) Match(msg *coap.Message, addr *net.UDPAddr, match *RouteMatch) 
 	return false
 }
 
+// NewRoute creates a new unconfigured Route
 func (r *Router) NewRoute() *Route {
 	route := &Route{}
 	r.routes = append(r.routes, route)
 	return route
 }
 
+// Handle creates a new Route with the given handler and a matcher for the
+// given path
 func (r *Router) Handle(path string, handler coap.Handler) *Route {
 	return r.NewRoute().Path(path).Handler(handler)
 }
 
+// Path creates a new Route with a matcher for the given path
 func (r *Router) Path(tpl string) *Route {
 	return r.NewRoute().Path(tpl)
 }
 
+// This method implements the interface for coap.Handler
 func (r *Router) ServeCOAP(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message {
 	var match *RouteMatch
 	var returnMessage *coap.Message
